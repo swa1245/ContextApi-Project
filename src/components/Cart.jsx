d676dae6fe1628bd972c62b1a4c3d8209b19c1f0
swa1245 <swarajpatil2123@gmail.com>
2025-03-23 09:08:22 +0530

    done

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SingleProduct from './SingleProduct'
 
 const Cart = ({cart, setCart}) => {
+  const [total,setTotal] = useState(0)
+  useEffect(()=>{
+    setTotal(cart.reduce((acc,curr)=>acc+Number(curr.price),0))
+  },[cart])
   return (
     <div>
       <span  className='text-2xl'>Cart</span>
-      <span className='text-2xl'>rs 100</span>
+      <span className='text-2xl'>Total: {total}</span>
       <div className=''>
         {
           cart.map((p)=>{ 
